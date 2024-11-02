ae0854af1337f3a92ebba898da86e998b3cfa1d3
Carlos. <carlos.pinedo@devo.com>
2024-11-02 09:31:40 +0000

    feature: Improved Services

--- components/Services/Services.tsx
+++ components/Services/Services.tsx
@@ -1,15 +1,18 @@
 import { services, Service } from './servicesList';
 
 export const Services = () => (
-  <div className="w-screen h-[100vh] relative mt-10 bg-[url('/background.jpg')] bg-cover bg-center min-h-screen flex items-center justify-center gap-5">
-    {services.map(({ id, name, description }: Service) => (
-      <div
-        key={id}
-        className="bg-primary p-10 flex flex-col justify-center items-center"
-      >
-        <span>{name}</span>
-        <p>{description}</p>
-      </div>
-    ))}
+  <div className="flex flex-col justify-around items-center bg-[url('/background.jpg')] w-screen relative mt-10 bg-cover bg-center min-h-screen">
+    <h1 className={'text-3xl mt-5'}>Servicios</h1>
+    <div className=" flex items-center justify-center gap-5 flex-wrap">
+      {services.map(({ id, name, description }: Service) => (
+        <div
+          key={id}
+          className="bg-primary p-10 flex flex-col justify-center items-center bg-opacity-80"
+        >
+          <span>{name}</span>
+          <p>{description}</p>
+        </div>
+      ))}
+    </div>
   </div>
 );
