963093046eaab8e77dc3ec147fd9cca046e0e374
Carlos. <carlos.pinedo@devo.com>
2024-11-02 10:20:07 +0000

    feature: Added About section in Home

---
+++ components/About/About.tsx
@@ -0,0 +1,6 @@
+export const About = () => (
+  <div className="flex flex-col justify-around items-center w-screen relative mt-10 min-h-screen">
+    <h1 className={'text-3xl mt-5'}>Sobre mí</h1>
+    <p>Soy una abogada que estudió Derecho en Madrid etc etc</p>
+  </div>
+);
--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,4 @@
 export * from './Navbar/Navbar';
 export * from './Contact/Contact';
 export * from './Services/Services';
+export * from './About/About';
