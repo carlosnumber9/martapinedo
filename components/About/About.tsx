0a66c9a780150286ce20b44cfe9d0b331461db5c
Carlos. <carlos.pinedo@devo.com>
2024-11-02 19:23:51 +0000

    feat: About sizing

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,5 +1,5 @@
 export const About = () => (
-  <div className="flex flex-col justify-around items-center w-screen relative mt-10 min-h-screen">
+  <div className="flex flex-col justify-around items-center w-screen relative mt-10 gap-10 pt-5 pb-5">
     <h1 className={'text-3xl mt-5'}>Sobre mí</h1>
     <p>Soy una abogada que estudió Derecho en Madrid etc etc</p>
   </div>
