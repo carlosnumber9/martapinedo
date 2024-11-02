f574414841f3f080754fea6828684d5c347eb651
Carlos. <carlos.pinedo@devo.com>
2024-11-02 18:29:42 +0000

    feat: Services sizing

--- components/Services/Services.tsx
+++ components/Services/Services.tsx
@@ -1,7 +1,7 @@
 import { services, Service } from './servicesList';
 
 export const Services = () => (
-  <div className="flex flex-col justify-around items-center bg-[url('/background.jpg')] w-screen relative mt-10 bg-cover bg-center min-h-screen">
+  <div className="flex flex-col justify-around items-center bg-[url('/background.jpg')] w-screen relative mt-10 bg-cover bg-center pt-5 pb-5 gap-10">
     <h1 className={'text-3xl mt-5'}>Servicios</h1>
     <div className=" flex items-center justify-center gap-5 flex-wrap">
       {services.map(({ id, name, description }: Service) => (
