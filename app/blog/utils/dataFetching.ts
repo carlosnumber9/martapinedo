0eba54dd0474edcb653a781d743e210d7466fa50
Carlos. <carlos.pinedo@devo.com>
2024-11-03 20:22:01 +0000

    fix: Lint

--- app/blog/utils/dataFetching.ts
+++ app/blog/utils/dataFetching.ts
@@ -1,13 +1,13 @@
 const fetchPosts = async () => {
-    try {
-      const headers = {
-        "content-type": "application/json",
-        Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
-        "gcms-locales": "es_ES",
-      };
+  try {
+    const headers = {
+      'content-type': 'application/json',
+      Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+      'gcms-locales': 'es_ES',
+    };
 
-      const requestBody = {
-        query: `query Posts {
+    const requestBody = {
+      query: `query Posts {
                 posts {
                   createdAt
                   id
@@ -22,21 +22,23 @@ const fetchPosts = async () => {
                   }
                 }
               }`,
-        variables: null,
-      };
+      variables: null,
+    };
 
-      const options = {
-        method: "POST",
-        headers,
-        body: JSON.stringify(requestBody),
-      };
+    const options = {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(requestBody),
+    };
 
-      const response = await (await fetch(process.env.CONTENT_URL ?? '' , options)).json();
+    const response = await (
+      await fetch(process.env.CONTENT_URL ?? '', options)
+    ).json();
 
-      return response?.data.posts;
-    } catch (e) {
-      console.error(e);
-    }
-  };
+    return response?.data.posts;
+  } catch (e) {
+    console.error(e);
+  }
+};
 
-  export default fetchPosts;
+export default fetchPosts;
