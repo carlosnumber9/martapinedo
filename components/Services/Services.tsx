40cc1e8dce384cc636cf21d5cb868beb778cbba6
Carlos. <carlos.pinedo@devo.com>
2024-11-09 13:09:17 +0000

    fix: Services responsiveness

--- components/Services/Services.tsx
+++ components/Services/Services.tsx
@@ -7,7 +7,7 @@ export const Services = () => (
       {services.map(({ id, name, description }: Service) => (
         <div
           key={id}
-          className="bg-darkSecondary p-10 flex flex-col justify-center items-center bg-opacity-80 sm:w-4/5 md:w-1/4 gap-5"
+          className="bg-darkSecondary p-10 flex flex-col justify-center items-center bg-opacity-80 sm:w-3/4 md:w-2/5 lg:w-1/4 gap-5 mx-5"
         >
           <h2 className="text-xl text-center">{name}</h2>
           <p className="text-justify">{description}</p>
