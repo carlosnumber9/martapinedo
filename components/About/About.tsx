3f623d0721d8b838dd9ed7062e65c2588b8d3b96
Carlos. <carlos.pinedo@devo.com>
2024-11-09 12:02:07 +0000

    feat: Added content for About

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,6 +1,17 @@
 export const About = () => (
   <div className="flex flex-col justify-around items-center w-screen relative gap-10 pt-5 pb-5 bg-darkSecondary">
     <h1 className={'text-3xl mt-5'}>Sobre mí</h1>
-    <p>Soy una abogada que estudió Derecho en Madrid etc etc</p>
+    <p className="p-10 text-justify">
+      Soy Marta Pinedo, abogada con experiencia en derecho civil y mercantil,
+      actualmente ejerciendo en EJO Abogados y en mi propio despacho
+      profesional. A lo largo de mi carrera he tenido la oportunidad de trabajar
+      en entornos de diversa índole, desde despachos familiares hasta grandes
+      empresas y juzgados, lo que me ha permitido desarrollarme en áreas como la
+      resolución de disputas, la gestión de procedimientos judiciales y la
+      revisión de contratos. Estas experiencias han fortalecido mi capacidad de
+      comunicación y mi enfoque en la atención al cliente. En mi tiempo libre,
+      soy una apasionada de la lectura y me esfuerzo por mantenerme en forma a
+      través de una rutina de gimnasio que me ayuda a equilibrar cuerpo y mente.{' '}
+    </p>
   </div>
 );
