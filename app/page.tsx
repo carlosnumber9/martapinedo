f56e573fdfea83dace95e80a5fd4b3ea418fa91e
Carlos. <cpinedocsb@gmail.com>
2025-04-05 22:35:16 +0000

    feature: Added 3D rotation on mouse move

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,33 @@
+"use client";
+
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import { About, Services } from '../components';
 import Link from 'next/link';
-
-export async function generateMetadata() {
-  return {
-    title: `Marta Pinedo Sánchez`,
-  };
-}
+import { motion } from 'framer-motion';
 
 const Page: React.FC = () => {
+  const [rotation, setRotation] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    document.title = "Marta Pinedo Sánchez";
+  }, []);
+
+  useEffect(() => {
+    const handleMouseMove = (event: MouseEvent) => {
+      const { clientX, clientY } = event;
+      const x = -((clientX / window.innerWidth) * 2 - 1) * 15;
+      const y = -((clientY / window.innerHeight) * 2 - 1) * -15;
+      setRotation({ x, y });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
   return (
     <>
       <div
@@ -16,13 +35,30 @@ const Page: React.FC = () => {
           'flex flex-col items-center justify-start flex-grow px-0 py-16'
         }
       >
-        <div className={'rounded-full w-max h-max overflow-hidden'}>
-          <Image
-            src={'/marta.jpeg'}
-            alt="Marta Pinedo Sánchez"
-            width={300}
-            height={300}
-          />
+        <div
+          className="w-max h-max overflow-hidden"
+          style={{
+            perspective: '1000px',
+          }}
+        >
+          <motion.div
+            style={{
+              transformStyle: 'preserve-3d',
+            }}
+            animate={{
+              rotateX: rotation.y,
+              rotateY: rotation.x,
+            }}
+            transition={{ type: 'spring', stiffness: 200, damping: 20 }}
+          >
+            <Image
+              className="rounded-full"
+              src={'/marta.jpeg'}
+              alt="Marta Pinedo Sánchez"
+              width={300}
+              height={300}
+            />
+          </motion.div>
         </div>
         <h1 className={'text-5xl md:text-4xl mt-5 text-center'}>
           Honestidad · Eficacia · Profesionalidad
