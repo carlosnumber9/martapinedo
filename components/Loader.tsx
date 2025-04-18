cdb08b298d32d1099d9743bcc0518947a3f5d667
Carlos. <cpinedocsb@gmail.com>
2025-04-18 10:00:01 +0000

    feat: Animation in Loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -1,9 +1,13 @@
+import { PageTransition } from './Transition';
+
 export const Loader = () => (
-  <div className="flex items-center justify-center h-screen bg-darkPrimary">
-    <div className="flex items-center justify-center space-x-2">
-      <div className="w-4 h-4 rounded-full animate-pulse bg-white/70"></div>
-      <div className="w-4 h-4 rounded-full animate-pulse bg-white/70"></div>
-      <div className="w-4 h-4 rounded-full animate-pulse bg-white/70"></div>
+  <PageTransition>
+    <div className="flex items-center justify-center h-screen bg-darkPrimary">
+      <div className="flex items-center justify-center space-x-2">
+        <div className="w-4 h-4 rounded-full animate-pulse bg-white/70"></div>
+        <div className="w-4 h-4 rounded-full animate-pulse bg-white/70"></div>
+        <div className="w-4 h-4 rounded-full animate-pulse bg-white/70"></div>
+      </div>
     </div>
-  </div>
+  </PageTransition>
 );
