6f89c33eefa589231bd6516c93bef0f54e086137
Carlos. <cpinedocsb@gmail.com>
2025-04-19 18:23:03 +0000

    feat: Changed Loading UI for post page

--- app/blog/[id]/loading.tsx
+++ app/blog/[id]/loading.tsx
@@ -1,9 +1,5 @@
-import { Loader } from 'components';
-
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center h-screen bg-transparent">
-      <Loader />
-    </div>
+    <div className="p-8 mt-5 flex flex-col items-center w-11/12 sm:w-3/4 bg-darkSecondary text-gray-200 h-screen animate-pulse" />
   );
 }
