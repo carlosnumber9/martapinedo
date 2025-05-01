1772ef9952bb51a6b6462411b8ad36b87353f570
Carlos. <cpinedocsb@gmail.com>
2025-05-01 18:09:18 +0000

    feat: Added link to EJO's firm

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,16 +7,25 @@ export const Footer = () => (
     aria-label="Footer"
   >
     <span>© Marta Pinedo Sánchez. Todos los derechos reservados.</span>
-    <div className="flex flex-row mt-5">
+    <div className="flex flex-row mt-5 items-center">
       <Link
         href={'https://www.linkedin.com/in/marta-pinedo-7a8959195'}
         target="_blank"
       >
         <Image
           src={'/linkedin.png'}
-          alt="LinkedIn logo with the link to Marta Pinedo LinkedIn profile"
+          alt="LinkedIn logo with linking to Marta Pinedo's LinkedIn profile"
           width={40}
           height={40}
+          className="mx-2 cursor-pointer hover:scale-105 transition-transform duration-200 ease-in-out"
+        />
+      </Link>
+      <Link href={'https://www.ejoabogados.com/'} target="_blank">
+        <Image
+          src={'/ejo.jpg'}
+          alt="EJO law firm logo linking to Marta Pinedo's current company"
+          width={50}
+          height={50}
           className="mx-2 cursor-pointer hover:scale-105 transition-transform duration-200 ease-in-out"
         />
       </Link>
