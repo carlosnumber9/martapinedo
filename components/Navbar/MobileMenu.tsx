1b8627dd61406ecee6ecb6753ee0dbd68c663a37
Carlos Pinedo <cpinedocsb@gmail.com>
2025-12-14 21:36:01 +0100

    feat: i18n for MobileMenu

--- components/Navbar/MobileMenu.tsx
+++ components/Navbar/MobileMenu.tsx
@@ -1,3 +1,5 @@
+import LanguageSwitcher from "components/LanguageSwitcher";
+import { useTranslations } from "next-intl";
 import Link from "next/link";
 
 interface Props {
@@ -5,17 +7,23 @@ interface Props {
     ref: React.RefObject<HTMLDivElement | null>;
 }
 
-export const MobileMenu: React.FC<Props> = ({ onClose, ref }) => (
-    <div ref={ref} className="mobile-menu sm:hidden fixed top-20 left-0 flex flex-col items-center justify-center bg-darkSecondary w-full shadow-lg z-40 h-full overflow-y-hidden">
-        <Link href="/blog" className="w-full h-15 p-5 border-solid border-t-2 border-b-2 border-darkPrimary" onClick={onClose}>
-            <div className="w-full h-15 p-15 flex items-center justify-center text-4xl">
-                Blog
+export const MobileMenu: React.FC<Props> = ({ onClose, ref }) => {
+    const t = useTranslations('navbar');
+    return (
+        <div ref={ref} className="mobile-menu sm:hidden fixed top-20 left-0 flex flex-col items-center justify-center bg-darkSecondary w-full shadow-lg z-40 h-full overflow-y-hidden">
+            <Link href="/blog" className="w-full h-15 p-5 border-solid border-t-2 border-b-2 border-darkPrimary" onClick={onClose}>
+                <div className="w-full h-15 p-15 flex items-center justify-center text-4xl">
+                    {t('buttons.blog')}
+                </div>
+            </Link>
+            <Link href="/contact" className="w-full h-15 p-5 border-solid border-b-2 border-darkPrimary" onClick={onClose}>
+                <div className="w-full h-15 p-15 flex items-center justify-center text-4xl">
+                    {t('buttons.contact')}
+                </div>
+            </Link>
+            <div className="w-full h-15 p-15 flex items-center justify-center text-4xl p-5">
+                <LanguageSwitcher />
             </div>
-        </Link>
-        <Link href="/contact" className="w-full h-15 p-5 border-solid border-b-2 border-darkPrimary" onClick={onClose}>
-            <div className="w-full h-15 p-15 flex items-center justify-center text-4xl">
-                Contacto
-            </div>
-        </Link>
-    </div>
-);
+        </div>
+    )
+};
