0b7a7ea457a83ac167cfd880169057b3bd039f70
Carlos Pinedo <cpinedocsb@gmail.com>
2025-12-21 13:04:42 +0100

    feat: Improved navbar buttons behavior on link click

--- components/Navbar/NavbarButton.tsx
+++ components/Navbar/NavbarButton.tsx
@@ -1,4 +1,5 @@
 import { Path } from 'app/types';
+import { on } from 'events';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
@@ -11,9 +12,10 @@ interface Props {
 
 export const NavbarButton: React.FC<Props> = ({ text, route, isMobile = false, onClick }) => {
   const path = usePathname();
+  const isAlreadyOnRoute = path === route;
   return (
-    <Link href={route} className={isMobile ? 'block' : 'hidden sm:block'} onClick={onClick}>
-      <div className={`h-full font-subtitle ${path === route ? 'text-blueSecondary' : 'text-white/90'} transition-colors duration-300 cursor-pointer ${isMobile ? 'text-4xl' : 'text-lg'}`}>{text}</div>
+    <Link href={route} className={isMobile ? 'block' : 'hidden sm:block'} onClick={() => isAlreadyOnRoute ? undefined : onClick?.()}>
+      <div className={`h-full font-subtitle ${isAlreadyOnRoute ? 'text-blueSecondary' : 'text-white/90'} transition-colors duration-300 cursor-pointer ${isMobile ? 'text-4xl' : 'text-lg'}`}>{text}</div>
     </Link>
   )
 };
