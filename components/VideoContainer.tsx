3759f1d95235e1c8772b28048d51f01bc0169828
Carlos Pinedo <cpinedocsb@gmail.com>
2026-06-06 20:26:12 +0200

    refactor: Replaced home background video with smaller copy and improved loading times. Assets cleanup

--- components/VideoContainer.tsx
+++ components/VideoContainer.tsx
@@ -9,12 +9,16 @@ export const VideoContainer: React.FC<Props> = ({ children }) => {
     <div className="fixed inset-0 z-0 h-screen w-full overflow-hidden">
       <video
         className="absolute inset-0 h-full w-full object-cover scale-150 blur-sm md:scale-100"
-        src="/night.mp4"
         autoPlay
         muted
         loop
         playsInline
-      />
+        preload="auto"
+        poster="/night-poster.jpg"
+      >
+        <source src="/night-hero.webm" type="video/webm" />
+        <source src="/night-hero.mp4" type="video/mp4" />
+      </video>
       <div className="relative h-screen w-full flex flex-row flex-wrap items-end justify-center">
         {children}
       </div>
