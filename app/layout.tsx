a332016447e0b6c90646bb3dafc3d2521d20b3c5
Carlos Pinedo <cpinedocsb@gmail.com>
2026-06-07 11:24:29 +0200

    feat: Updated RootLayout to use dynamic locale for language attribute

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,11 @@ import { AnalyticsProvider, CookieConsentBanner, Footer, Navbar } from 'componen
 import StructuredData from 'components/StructuredData';
 import { Metadata } from 'next';
 import { NextIntlClientProvider } from 'next-intl';
-import { getTranslations } from 'next-intl/server';
+import { getLocale, getTranslations } from 'next-intl/server';
 import { PropsWithChildren } from 'react';
 import { bodyFont, mainFont, subtitleFont } from 'utils/fonts';
 import { buildPageMetadata, SITE_NAME, SITE_URL } from 'utils/seo';
+import { SupportedLocale } from './types';
 import '../styles/globals.css';
 
 export async function generateMetadata(): Promise<Metadata> {
@@ -30,21 +31,25 @@ export async function generateMetadata(): Promise<Metadata> {
 
 type Props = PropsWithChildren<{}>;
 
-const RootLayout: React.FC<Props> = ({ children }) => (
-  <html lang="en" className={`${mainFont.variable} ${subtitleFont.variable} ${bodyFont.variable}`}>
-    <body className="text-white/90 bg-darkPrimary min-h-screen flex flex-col font-body select-none">
-      <NextIntlClientProvider>
-        <header>
-          <Navbar />
-        </header>
-        <AnalyticsProvider />
-        <StructuredData />
-        <main className="py-0 flex flex-col items-center flex-1 w-full">{children}</main>
-        <Footer />
-        <CookieConsentBanner />
-      </NextIntlClientProvider>
-    </body>
-  </html>
-);
+const RootLayout = async ({ children }: Props) => {
+  const locale = (await getLocale()) as SupportedLocale;
+
+  return (
+    <html lang={locale} className={`${mainFont.variable} ${subtitleFont.variable} ${bodyFont.variable}`}>
+      <body className="text-white/90 bg-darkPrimary min-h-screen flex flex-col font-body select-none">
+        <NextIntlClientProvider>
+          <header>
+            <Navbar />
+          </header>
+          <AnalyticsProvider />
+          <StructuredData />
+          <main className="py-0 flex flex-col items-center flex-1 w-full">{children}</main>
+          <Footer />
+          <CookieConsentBanner />
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+};
 
 export default RootLayout;
